5a61f8efab5295dc9630963288c8977e7d560f27
Zhakusama <yowanehakusama@qq.com>
2019-07-13 20:42:23 +0800

    Site updated: 2019-07-13 20:42:16

---
+++ js/src/hitokoto.js
@@ -0,0 +1,18 @@
+window.onload=function () {
+    var hitokoto = document.querySelector('.hitokoto');
+    var from = document.querySelector('.from');
+    update();
+    function update() {
+        gethi = new XMLHttpRequest();
+        gethi.open("GET","https://v1.hitokoto.cn/");
+        //这里选择类别，详见官方文档
+        gethi.send();
+        gethi.onreadystatechange = function () {
+            if (gethi.readyState===4 && gethi.status===200) {
+                var Hi = JSON.parse(gethi.responseText);
+                hitokoto.innerHTML = Hi.hitokoto;
+                from.innerHTML = "——" + Hi.from; //可自定义输出格式
+            }
+        }
+    }
+}
